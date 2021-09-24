43142bffd50f94ccafa0d9a15797066b27c84c69
Iuri Magnago <iurimp91@gmail.com>
2021-09-24 19:58:00 -0300

    feat: connection with challenge's api

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 app.use(cors());
+
+const data = [];
+
+const request = axios.get("http://challenge.dienekes.com.br/api/numbers?page=1");
+
+request.then(response => {
+    data.push(response.data);
+});
+
+request.catch(error => {
+    console.log("Something went wrong.");
+});
 
 app.get("/", (req, res) => {
     res.send("Hello");
