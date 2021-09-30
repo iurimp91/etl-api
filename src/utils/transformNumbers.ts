6e62b735fdb175c6fe39655656a1745dc37f93f4
Iuri Magnago <iurimp91@gmail.com>
2021-09-29 21:58:13 -0300

    refactor: transformNumbers function

--- src/utils/transformNumbers.ts
+++ src/utils/transformNumbers.ts
@@ -1,21 +1,29 @@
 export default function transformNumbers(numbers: number[]) {
     for(let i = 0; i < numbers.length; i++) {
         let j = i;
-        while(numbers[j] > numbers[j+1]) {
-            let temp = numbers[j+1];
-            numbers[j+1] = numbers[j];
-            numbers[j] = temp;
-            j++;
-        }
+        rightIteration(j, numbers);
 
         j = i;
-        while(numbers[j] < numbers[j-1]) {
-            let temp = numbers[j-1];
-            numbers[j-1] = numbers[j];
-            numbers[j] = temp;
-            j--;
-        }
+        leftIteration(j, numbers);
     }
 
     return numbers;
+}
+
+function rightIteration(j: number, numbers: number[]) {
+    while(numbers[j] > numbers[j+1]) {
+        let temp = numbers[j+1];
+        numbers[j+1] = numbers[j];
+        numbers[j] = temp;
+        j++;
+    }
+}
+
+function leftIteration(j: number, numbers: number[]) {
+    while(numbers[j] < numbers[j-1]) {
+        let temp = numbers[j-1];
+        numbers[j-1] = numbers[j];
+        numbers[j] = temp;
+        j--;
+    }
 }
